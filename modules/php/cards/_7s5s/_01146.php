670e2be89bf569646550ca22b1d6fabb5fb95b1d
Edward Mittelstedt <bugbucket@comcast.net>
2024-11-25 14:43:40 -0500

    Let the Sword Decide Scheme Resolution

--- modules/php/cards/_7s5s/_01146.php
+++ modules/php/cards/_7s5s/_01146.php
@@ -2,7 +2,12 @@
 
 namespace Bga\Games\SeventhSeaCityOfFiveSails\cards\_7s5s;
 
+use Bga\Games\SeventhSeaCityOfFiveSails\Game;
 use Bga\Games\SeventhSeaCityOfFiveSails\cards\Scheme;
+use Bga\Games\SeventhSeaCityOfFiveSails\theah\Events;
+use Bga\Games\SeventhSeaCityOfFiveSails\theah\events\Event;
+use Bga\Games\SeventhSeaCityOfFiveSails\theah\events\EventReknownAddedToLocation;
+use Bga\Games\SeventhSeaCityOfFiveSails\theah\events\EventResolveScheme;
 
 class _01146 extends Scheme
 {
@@ -23,5 +28,35 @@ class _01146 extends Scheme
             "Flourish", 
             "Honor",
         ];
+    }
+
+    public function handleEvent($event)
+    {
+        parent::handleEvent($event);
+
+        if ($event instanceof EventResolveScheme && $event->scheme->Id == $this->Id) {
+
+            $event->theah->game->notifyAllPlayers("schemeResolves", clienttranslate('${scheme_name} now resolves.  Reknown will be added to The Docks and The Forum.'), [
+                "scheme_name" => "<span style='font-weight:bold'>{$this->Name}</span>",
+            ]);
+
+            $reknown = $event->theah->createEvent(Events::ReknownAddedToLocation);
+            if ($reknown instanceof EventReknownAddedToLocation) {
+                $reknown->location = Game::LOCATION_CITY_DOCKS;
+                $reknown->amount = 1;
+                $reknown->priority = Event::HIGH_PRIORITY;
+                $reknown->source = $this->Name;
+            }
+            $event->theah->queueEvent($reknown);
+
+            $reknown = $event->theah->createEvent(Events::ReknownAddedToLocation);
+            if ($reknown instanceof EventReknownAddedToLocation) {
+                $reknown->location = Game::LOCATION_CITY_FORUM;
+                $reknown->amount = 1;
+                $reknown->priority = Event::HIGH_PRIORITY;
+                $reknown->source = $this->Name;
+            }
+            $event->theah->queueEvent($reknown);
+        }
     }
 }
