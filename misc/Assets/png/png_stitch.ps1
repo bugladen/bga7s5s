e76f03e88c99483423ed0ede6ff6af2803b445fa
Edward Mittelstedt <bugbucket@comcast.net>
2025-01-09 13:22:37 -0500

    Added icons for 3-4 player locations

--- misc/Assets/png/png_stitch.ps1
+++ misc/Assets/png/png_stitch.ps1
@@ -1,5 +1,5 @@
 magick '.\the docks.png' .\location-forum.png '.\grande bazaar.png' +append '.\city symbols.png'
-magick '.\city symbols.png' '.\Crew Cap.png' .\Panache.png .\nail.png locker.png discard.png hand.png +append step1.png
+magick '.\city symbols.png' '.\Crew Cap.png' .\Panache.png .\nail.png locker.png discard.png hand.png '.\oles inn.png' '.\gov gardens.png' +append step1.png
 magick .\step1.png .\seals.png -append step2.png
 magick .\step2.png CFI.png +append step3.png
 magick seal.png initiative.png cost.png +append step4.png
