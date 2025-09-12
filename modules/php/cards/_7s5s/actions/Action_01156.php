c02c25ded8f27ee80c74447719ed73da53a09bb1
Edward Mittelstedt <bugbucket@comcast.net>
2025-09-12 07:57:18 -0400

    Matchlock Musket - now correctly selects target characters

--- modules/php/cards/_7s5s/actions/Action_01156.php
+++ modules/php/cards/_7s5s/actions/Action_01156.php
@@ -72,9 +72,11 @@ class Action_01156 extends AttachmentAction
             foreach ($adjacentLocations as $adjacentLocation)
             {
                 $opposingCharacters = $game->theah->getCharactersAtLocation($adjacentLocation);
-                $opposingCharacter = array_values(array_filter($opposingCharacters, fn($c) => $c->isNotControlledByPlayer($performer->ControllerId)));
-                if (count($opposingCharacter) > 0)
-                    $charactersIds[] = $opposingCharacter[0]->Id;
+                $opposingCharacters = array_filter($opposingCharacters, fn($c) => $c->isNotControlledByPlayer($performer->ControllerId));
+                foreach ($opposingCharacters as $opposingCharacter)
+                {
+                    $charactersIds[] = $opposingCharacter->Id;
+                }
             }
 
             $charactersIds = array_unique($charactersIds);
