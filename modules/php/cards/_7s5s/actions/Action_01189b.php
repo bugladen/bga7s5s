75bb9fd04e5ffd91bbd48f0410a7607672b652bf
Edward Mittelstedt <bugbucket@comcast.net>
2025-09-16 06:25:18 -0400

    Point of Opportunity - Action now engages Performer

--- modules/php/cards/_7s5s/actions/Action_01189b.php
+++ modules/php/cards/_7s5s/actions/Action_01189b.php
@@ -98,6 +98,9 @@ class Action_01189b extends EventCityAction
             {
                 throw new \BgaUserException(sprintf($game->translate("%s does not have any reknown to move."), $poo->Location));
             }
+
+            $engageEvent = EventFactory::createCardEngagedEvent($performer->ControllerId, $performer->Id, $poo->Id);
+            $game->theah->eventCheck($engageEvent);
     
             $fromEvent = EventFactory::createReknownRemovedFromLocationEvent($performer->ControllerId, $poo->Location, 1, "{$poo->getInjectCode()}: Moving Reknown to adjacent location");
             $game->theah->eventCheck($fromEvent);
@@ -108,6 +111,7 @@ class Action_01189b extends EventCityAction
             $discardEvent = EventFactory::createCardAddedToCityDiscardPileEvent($poo->ControllerId, $poo->Id, $poo->Location);
             $game->theah->eventCheck($discardEvent);
     
+            $game->theah->queueEvent($engageEvent);
             $game->theah->queueEvent($fromEvent);
             $game->theah->queueEvent($toEvent);
             $game->theah->queueEvent($discardEvent);
