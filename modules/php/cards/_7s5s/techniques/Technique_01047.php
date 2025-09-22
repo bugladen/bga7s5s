428d026b0c07346bb04d941e2d62f22fc7d65436
Edward Mittelstedt <bugbucket@comcast.net>
2025-09-21 22:12:36 -0400

    Kaspar's Panzerhand - Technique now only show as option if conditions exist

--- modules/php/cards/_7s5s/techniques/Technique_01047.php
+++ modules/php/cards/_7s5s/techniques/Technique_01047.php
@@ -3,8 +3,10 @@
 namespace Bga\Games\SeventhSeaCityOfFiveSails\cards\_7s5s\techniques;
 
 use Bga\Games\SeventhSeaCityOfFiveSails\cards\techniques\Technique;
+use Bga\Games\SeventhSeaCityOfFiveSails\Game;
 use Bga\Games\SeventhSeaCityOfFiveSails\theah\events\Event;
 use Bga\Games\SeventhSeaCityOfFiveSails\theah\events\EventDuelCalculateTechniqueValues;
+use Bga\Games\SeventhSeaCityOfFiveSails\theah\Theah;
 
 class Technique_01047 extends Technique
 {
@@ -12,6 +14,28 @@ class Technique_01047 extends Technique
     {
         parent::__construct();
         $this->Name = clienttranslate("+1 Parry if Melee Weapon Equipped");
+    }
+
+    public function isAvailableToPlayer(int $playerId, Theah $theah): bool
+    {
+        if (! parent::isAvailableToPlayer($playerId, $theah))
+            return false;
+        
+        $inDuel = $theah->game->globals->get(Game::IN_DUEL, false);
+        if (!$inDuel)
+            return false;
+
+        $owningCharacter = $this->getOwningCharacter($theah);
+        foreach ($owningCharacter->Attachments as $attachmentId)
+        {
+            $attachment = $theah->getAttachmentById($attachmentId);
+            if ($attachment->hasTrait("Melee") && $attachment->hasTrait("Weapon"))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public function handleEvent(Event $event)
