584c7edccc83bb5de0bf46c4278ec6c9b3c334c2
Edward Mittelstedt <bugbucket@comcast.net>
2025-09-27 07:40:40 -0400

    Castille  deck picker and starter deck activated

--- modules/php/includes/starterdecks.inc.php
+++ modules/php/includes/starterdecks.inc.php
@@ -4,6 +4,41 @@ $this->starter_decks = <<<JSON
 {
     "decks": [
         {
+            "id": "SDCastille",
+            "name": "Castille Starter",
+            "faction": "Castille",
+            "leader": "01089",
+            "approach_deck": [ "01098", "01099", "01148", "01149", "01150", "01091", "01092", "01093", "01094", "01097" ],
+            "faction_deck": [
+                { "id": "01100", "count": 1 },
+                { "id": "01101", "count": 2 },
+                { "id": "01102", "count": 2 },
+                { "id": "01103", "count": 1 },
+                { "id": "01104", "count": 2 },
+                { "id": "01105", "count": 2 },
+                { "id": "01106", "count": 2 },
+                { "id": "01107", "count": 1 },
+                { "id": "01108", "count": 2 },
+                { "id": "01109", "count": 2 },
+                { "id": "01110", "count": 1 },
+                { "id": "01111", "count": 1 },
+                { "id": "01112", "count": 2 },
+                { "id": "01113", "count": 2 },
+                { "id": "01114", "count": 2 },
+                { "id": "01115", "count": 2 },
+                { "id": "01156", "count": 1 },
+                { "id": "01157", "count": 2 },
+                { "id": "01158", "count": 2 },
+                { "id": "01164", "count": 1 },
+                { "id": "01165", "count": 1 },
+                { "id": "01167", "count": 1 },
+                { "id": "01168", "count": 1 },
+                { "id": "01169", "count": 2 },
+                { "id": "01171", "count": 1 },
+                { "id": "01176", "count": 1 }
+            ]
+        },
+        {
             "id": "SDEisen",
             "name": "Eisen Starter",
             "faction": "Eisen",
@@ -102,41 +137,6 @@ $this->starter_decks = <<<JSON
 JSON;
 
 /*
-        {
-            "id": "SDCastille",
-            "name": "Castille Starter",
-            "faction": "Castille",
-            "leader": "01089",
-            "approach_deck": [ "01098", "01099", "01148", "01149", "01150", "01091", "01092", "01093", "01094", "01097" ],
-            "faction_deck": [
-                { "id": "01100", "count": 1 },
-                { "id": "01101", "count": 2 },
-                { "id": "01102", "count": 2 },
-                { "id": "01103", "count": 1 },
-                { "id": "01104", "count": 2 },
-                { "id": "01105", "count": 2 },
-                { "id": "01106", "count": 2 },
-                { "id": "01107", "count": 1 },
-                { "id": "01108", "count": 2 },
-                { "id": "01109", "count": 2 },
-                { "id": "01110", "count": 1 },
-                { "id": "01111", "count": 1 },
-                { "id": "01112", "count": 2 },
-                { "id": "01113", "count": 2 },
-                { "id": "01114", "count": 2 },
-                { "id": "01115", "count": 2 },
-                { "id": "01156", "count": 1 },
-                { "id": "01157", "count": 2 },
-                { "id": "01158", "count": 2 },
-                { "id": "01164", "count": 1 },
-                { "id": "01165", "count": 1 },
-                { "id": "01167", "count": 1 },
-                { "id": "01168", "count": 1 },
-                { "id": "01169", "count": 2 },
-                { "id": "01171", "count": 1 },
-                { "id": "01176", "count": 1 }
-            ]
-        },
         {
             "id": "SDUsurra",
             "name": "Usurra Starter",
