2ebe351d7827656e8a188ceda6215a9cedeed7d3
Edward Mittelstedt <bugbucket@comcast.net>
2025-10-22 06:51:32 -0400

    Revert Takama fix

--- modules/php/cards/reactions/Reaction_01199.php
+++ modules/php/cards/reactions/Reaction_01199.php
@@ -69,9 +69,6 @@ class Reaction_01199 extends CardReaction
                 {
                     $transition = EventFactory::createReactionTransitionEvent($takama->ControllerId, $takama->Id, $this->Id);
                     $event->queueEvent($transition);
-
-                    $this->TargetCharacterId = 0;
-                    $takama->IsUpdated = true;
                 }
             }
         }   
