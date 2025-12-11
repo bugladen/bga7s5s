ba89522c286cddf3446b9d4ba0f3a2a52427a54b
Edward Mittelstedt <bugbucket@comcast.net>
2025-12-11 12:21:43 -0500

    Pull - was not identifying itself as a Sorcerer ability (for Maxime's immunity)

--- modules/php/cards/_7s5s/actions/Action_01172.php
+++ modules/php/cards/_7s5s/actions/Action_01172.php
@@ -3,6 +3,7 @@
 namespace Bga\Games\SeventhSeaCityOfFiveSails\cards\_7s5s\actions;
 
 use Bga\Games\SeventhSeaCityOfFiveSails\cards\actions\RiskAction;
+use Bga\Games\SeventhSeaCityOfFiveSails\cards\ISorcererAbility;
 use Bga\Games\SeventhSeaCityOfFiveSails\cards\IAbilityThatTargetsCharacters;
 use Bga\Games\SeventhSeaCityOfFiveSails\EventFactory;
 use Bga\Games\SeventhSeaCityOfFiveSails\Game;
@@ -11,7 +12,7 @@ use Bga\Games\SeventhSeaCityOfFiveSails\theah\events\Event;
 use Bga\Games\SeventhSeaCityOfFiveSails\theah\events\EventActionTriggered;
 use Bga\Games\SeventhSeaCityOfFiveSails\theah\Theah;
 
-class Action_01172 extends RiskAction implements IAbilityThatTargetsCharacters
+class Action_01172 extends RiskAction implements ISorcererAbility, IAbilityThatTargetsCharacters
 {
     public function __construct()
     {
