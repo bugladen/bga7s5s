0a5c9d834f21bafb79be4d47a778863d8b60c7a5
Edward Mittelstedt <bugbucket@comcast.net>
2026-01-01 08:30:27 -0500

    Duckfoot Pistol - certain edge use-cases were causing a server error

--- modules/php/cards/_7s5s/actions/Action_01191.php
+++ modules/php/cards/_7s5s/actions/Action_01191.php
@@ -3,8 +3,6 @@
 namespace Bga\Games\SeventhSeaCityOfFiveSails\cards\_7s5s\actions;
 
 use Bga\Games\SeventhSeaCityOfFiveSails\cards\actions\AttachmentAction;
-use Bga\Games\SeventhSeaCityOfFiveSails\cards\Attachment;
-use Bga\Games\SeventhSeaCityOfFiveSails\cards\Leader;
 use Bga\Games\SeventhSeaCityOfFiveSails\EventFactory;
 use Bga\Games\SeventhSeaCityOfFiveSails\theah\events\Event;
 use Bga\Games\SeventhSeaCityOfFiveSails\theah\events\EventActionTriggered;
@@ -25,16 +23,10 @@ class Action_01191 extends AttachmentAction
         if ($event instanceof EventActionTriggered && $event->actionId == $this->Id)
         {
             $duckfootPistol = $this->getOwningCard($event->theah);
-            $ownerId = 0;
-            if ($duckfootPistol instanceof Attachment)
-            {
-                $ownerId = $duckfootPistol->AttachedToId;
-            }
-
-            $owner = $event->theah->getCardById($ownerId);
+            $owner = $this->getOwningCharacter($event->theah);
             $location = $owner->Location;
 
-            $event->theah->game->notifyAllPlayers("duckfootPistolUsed", clienttranslate('${player_name} uses ${card_name} to wound all Non-Leader characters at ${location}'), [
+            $event->theah->game->notify->all("duckfootPistolUsed", clienttranslate('${player_name} uses ${card_name} to wound all Non-Leader characters at ${location}'), [
                 'i18n' => ['card_name', 'location'],
                 'player_name' => $event->theah->game->getActivePlayerName(),
                 'card_name' => $duckfootPistol->Name,
@@ -43,9 +35,9 @@ class Action_01191 extends AttachmentAction
 
             $characters = $event->theah->getCharactersAtLocation($location);
             // Filter out characters are not leaders
-            $characters = array_filter($characters, fn($character) => ! $character instanceof Leader);
+            $characters = array_filter($characters, fn($character) => ! $character->hasTrait("Leader"));
 
-            $unequippedEvent = EventFactory::createAttachmentUnequippedEvent($event->playerId, $ownerId, $duckfootPistol->Id);
+            $unequippedEvent = EventFactory::createAttachmentUnequippedEvent($event->playerId, $owner->Id, $duckfootPistol->Id);
             $event->theah->queueEvent($unequippedEvent);
 
             $discardEvent = EventFactory::createCardAddedToCityDiscardPileEvent($event->playerId, $duckfootPistol->Id, $location, $duckfootPistol->Id, $asEffect = false);
@@ -57,7 +49,7 @@ class Action_01191 extends AttachmentAction
                 $event->theah->queueEvent($woundEvent);
             }
 
-            $actionResolvedEvent = EventFactory::createActionResolvedEvent($character->ControllerId);
+            $actionResolvedEvent = EventFactory::createActionResolvedEvent($owner->ControllerId);
             $event->theah->queueEvent($actionResolvedEvent);
 
             $this->resetPlayerPassCount($event->theah->game);
