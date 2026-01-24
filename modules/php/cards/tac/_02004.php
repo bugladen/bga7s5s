276e565a95a09e48ac03e47c7d5019955be9f1a4
Edward Mittelstedt <bugbucket@comcast.net>
2026-01-24 11:37:17 -0500

    Crash the Party

---
+++ modules/php/cards/tac/_02004.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Bga\Games\SeventhSeaCityOfFiveSails\cards\tac;
+
+use Bga\Games\SeventhSeaCityOfFiveSails\cards\IHasReactions;
+use Bga\Games\SeventhSeaCityOfFiveSails\cards\ReactionTrait;
+use Bga\Games\SeventhSeaCityOfFiveSails\cards\Scheme;
+use Bga\Games\SeventhSeaCityOfFiveSails\EventFactory;
+use Bga\Games\SeventhSeaCityOfFiveSails\Game;
+use Bga\Games\SeventhSeaCityOfFiveSails\theah\events\Event;
+use Bga\Games\SeventhSeaCityOfFiveSails\theah\events\EventResolveScheme;
+use Bga\Games\SeventhSeaCityOfFiveSails\cards\tac\reactions\Reaction_02004;
+
+class _02004 extends Scheme implements IHasReactions
+{
+    use ReactionTrait;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->Name = clienttranslate('Crash the Party');
+        $this->Image = '02004.jpg';
+        $this->ExpansionName = 'tac';
+        $this->ExpansionNumber = 2;
+        $this->CardNumber = 4;
+
+        $this->initializeFaction('Vodacce');
+        $this->Initiative = 12;
+        $this->PanacheModifier = 0;
+
+        $this->Traits = [
+            'Ad Hoc',
+            'Unwelcome',
+        ];
+
+        $this->resetCard();
+
+        $this->Reactions = [
+            new Reaction_02004(),
+        ];
+    }
+
+    public function handleEvent(Event $event)
+    {
+        parent::handleEvent($event);
+
+        if ($event instanceof EventResolveScheme && $event->scheme->Id == $this->Id)
+        {
+            $event->theah->game->notify->all("message", clienttranslate('${scheme_inject_code} now resolves.  Renown will be added to The Docks and The Grand Bazaar.'), [
+                "scheme_inject_code" => $this->getInjectCode(),
+            ]);
+
+            $reknown = EventFactory::createReknownAddedToLocationEvent($this->ControllerId, Game::LOCATION_CITY_DOCKS, 1, $this->getInjectCode());
+            $event->theah->queueEvent($reknown);
+
+            $reknown = EventFactory::createReknownAddedToLocationEvent($this->ControllerId, Game::LOCATION_CITY_BAZAAR, 1, $this->getInjectCode());
+            $event->theah->queueEvent($reknown);
+        }
+    }
+}
