d67d8c98f5aaac56c0ad40bd887bfe436e24b052
Edward Mittelstedt <bugbucket@comcast.net>
2026-02-20 06:18:13 -0500

    Added zombieModeLevel

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -92,4 +92,6 @@ $gameinfos = [
         //  minimum possible value: 320 (the lowest value you specify, the better the display is on mobile)
         'min' => 740,
     ],
+
+    'zombieModeLevel'=> 0,
 ];
