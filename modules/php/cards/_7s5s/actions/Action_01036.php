4d8f070bcedad9399eeec58396854cd6137d8db8
Edward Mittelstedt <bugbucket@comcast.net>
2026-03-01 10:05:19 -0500

    Updated Daniella Dietrich for valid targets

--- modules/php/cards/_7s5s/actions/Action_01036.php
+++ modules/php/cards/_7s5s/actions/Action_01036.php
@@ -3,6 +3,7 @@
 namespace Bga\Games\SeventhSeaCityOfFiveSails\cards\_7s5s\actions;
 
 use Bga\Games\SeventhSeaCityOfFiveSails\cards\actions\CharacterAction;
+use Bga\Games\SeventhSeaCityOfFiveSails\cards\Character;
 use Bga\Games\SeventhSeaCityOfFiveSails\cards\IAbilityThatTargetsCards;
 use Bga\Games\SeventhSeaCityOfFiveSails\cards\IAbilityThatTargetsCharacters;
 use Bga\Games\SeventhSeaCityOfFiveSails\EventFactory;
@@ -56,6 +57,11 @@ class Action_01036 extends CharacterAction implements IAbilityThatTargetsCards, 
             $performer->canChallenge());
 
         return $performers;
+    }
+
+    public function isValidTargetForAbility(Game $game, Character $character): array
+    {
+        return [true, ""];
     }
 
     public function handleEvent(Event $event)
