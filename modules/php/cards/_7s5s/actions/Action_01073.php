710e5307527cbca295e84d993b344bf1890aa119
Edward Mittelstedt <bugbucket@comcast.net>
2026-03-04 17:21:38 -0500

    Updated Cavalier Hat for valid targets

--- modules/php/cards/_7s5s/actions/Action_01073.php
+++ modules/php/cards/_7s5s/actions/Action_01073.php
@@ -3,6 +3,7 @@
 namespace Bga\Games\SeventhSeaCityOfFiveSails\cards\_7s5s\actions;
 
 use Bga\Games\SeventhSeaCityOfFiveSails\cards\actions\AttachmentAction;
+use Bga\Games\SeventhSeaCityOfFiveSails\cards\Character;
 use Bga\Games\SeventhSeaCityOfFiveSails\cards\IAbilityThatTargetsCards;
 use Bga\Games\SeventhSeaCityOfFiveSails\cards\IAbilityThatTargetsCharacters;
 use Bga\Games\SeventhSeaCityOfFiveSails\EventFactory;
@@ -49,6 +50,24 @@ class Action_01073 extends AttachmentAction implements IAbilityThatTargetsCards,
         return count($characters) > 0;
     }
 
+    public function isValidTargetForAbility(Game $game, Character $character): array
+    {
+        $performerId = $game->globals->get(Game::CHOSEN_PERFORMER);
+        $performer = $game->theah->getCharacterById($performerId);
+
+        if ($character->ControllerId == $performer->ControllerId)
+        {
+            return [false, $game->translate("You cannot challenge a character that is controlled by you.")];
+        }
+
+        if ($character->Location != $performer->Location)
+        {
+            return [false, $game->translate("Character is not at the same location as the performer")];
+        }
+
+        return [true, ""];
+    }
+
     public function handleEvent(Event $event)
     {
         parent::handleEvent($event);
@@ -67,6 +86,7 @@ class Action_01073 extends AttachmentAction implements IAbilityThatTargetsCards,
 
             //resetPlayerPassCount is called in stSetupChallenge
             // $this->setUsed() is called in stSetupChallenge
+            // createActionResolvedEvent() is called when the challenge is resolved
         }
     }
 }
