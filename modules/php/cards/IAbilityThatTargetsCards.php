91ffa16e1879f149eb9fd846a596a1bb4b0ca216
Edward Mittelstedt <bugbucket@comcast.net>
2026-03-07 18:11:33 -0500

    Reverted change

--- modules/php/cards/IAbilityThatTargetsCards.php
+++ modules/php/cards/IAbilityThatTargetsCards.php
@@ -2,9 +2,4 @@
 
 namespace Bga\Games\SeventhSeaCityOfFiveSails\cards;
 
-use Bga\Games\SeventhSeaCityOfFiveSails\Game;
-
-interface IAbilityThatTargetsCards 
-{
-    function isValidTargetForAbility(Game $game, Character $character): array;
-}
+interface IAbilityThatTargetsCards {}
