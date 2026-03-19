15cff504be9f4e9448643d78707c8baadcc2ba89
Edward Mittelstedt <bugbucket@comcast.net>
2026-03-19 14:05:31 -0400

    Divine Discipline

---
+++ modules/php/cards/tac/_02018.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Bga\Games\SeventhSeaCityOfFiveSails\cards\tac;
+
+use Bga\Games\SeventhSeaCityOfFiveSails\cards\ActionTrait;
+use Bga\Games\SeventhSeaCityOfFiveSails\cards\IHasActions;
+use Bga\Games\SeventhSeaCityOfFiveSails\cards\IHasManeuvers;
+use Bga\Games\SeventhSeaCityOfFiveSails\cards\ManeuverTrait;
+use Bga\Games\SeventhSeaCityOfFiveSails\cards\Risk;
+use Bga\Games\SeventhSeaCityOfFiveSails\cards\tac\actions\Action_02018;
+use Bga\Games\SeventhSeaCityOfFiveSails\cards\tac\maneuvers\Maneuver_02018;
+
+class _02018 extends Risk implements IHasActions, IHasManeuvers
+{
+    use ActionTrait;
+    use ManeuverTrait;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->Name = clienttranslate("Divine Discipline");
+        $this->Image = "02018.jpg";
+        $this->ExpansionName = "tac";
+        $this->ExpansionNumber = 2;
+        $this->CardNumber = 18;
+
+        $this->initializeFaction("Eisen");
+
+        $this->Riposte = 0;
+        $this->DashedRiposte = true;
+
+        $this->Parry = 1;
+        $this->Thrust = 3;
+
+        $this->WealthCost = 1;
+
+        $this->Traits = [
+            "Flourish",
+            "Faith",
+            "Penance",
+        ];
+
+        $this->resetCard();
+
+        $this->Actions = [
+            new Action_02018(),
+        ];
+
+        $this->Maneuvers = [
+            new Maneuver_02018(),
+        ];
+    }
+}
---
+++ modules/php/cards/tac/actions/Action_02018.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Bga\Games\SeventhSeaCityOfFiveSails\cards\tac\actions;
+
+use Bga\Games\SeventhSeaCityOfFiveSails\cards\actions\RiskAction;
+use Bga\Games\SeventhSeaCityOfFiveSails\EventFactory;
+use Bga\Games\SeventhSeaCityOfFiveSails\Game;
+use Bga\Games\SeventhSeaCityOfFiveSails\theah\events\Event;
+use Bga\Games\SeventhSeaCityOfFiveSails\theah\events\EventActionTriggered;
+use Bga\Games\SeventhSeaCityOfFiveSails\theah\Theah;
+
+class Action_02018 extends RiskAction
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->Name = clienttranslate("Wound all Characters at your Performer's Location");
+        $this->RequiresPerformerSelected = true;
+    }
+
+    public function isAvailableToPlayer(int $playerId, Theah $theah, bool $overrideInHandCheck = false): bool
+    {
+        if ( ! parent::isAvailableToPlayer($playerId, $theah, $overrideInHandCheck))
+        {
+            return false;
+        }
+
+        $characters = $theah->getCharactersInPlayByPlayerId($playerId);
+        $characters = array_filter($characters, fn($character) => 
+            $theah->cardInCity($character) && $character->hasTrait("Zealot"));
+        return count($characters) > 0;
+    }
+
+
+    public function getPerformersForAction(int $playerId, Theah $theah): array
+    {
+        $performers = parent::getPerformersForAction($playerId, $theah);
+        $performers = array_values(array_filter($performers, fn($performer) => 
+            $theah->cardInCity($performer) && $performer->hasTrait("Zealot")));
+        return $performers;
+    }
+
+    public function handleEvent(Event $event)
+    {
+        parent::handleEvent($event);
+
+        if ($event instanceof EventActionTriggered && $event->actionId == $this->Id)
+        {
+            $owner = $this->getOwningCard($event->theah);
+            $performerId = $event->theah->game->globals->get(Game::CHOSEN_PERFORMER);
+            $performer = $event->theah->getCharacterById($performerId);
+
+            $this->setUsed($event->theah, true);
+            $this->announceAction($event->theah->game);
+            $this->resetPlayerPassCount($event->theah->game);
+
+            $characters = $event->theah->getCharactersAtLocation($performer->Location);
+            foreach ($characters as $character)
+            {
+                $woundEvent = EventFactory::createCharacterBeingWoundedEvent($character->Id, $owner->Id, 1, $owner->getInjectCode(), $this->Id);
+                $event->theah->queueEvent($woundEvent);
+            }
+
+            $actionResolvedEvent = EventFactory::createActionResolvedEvent($owner->ControllerId);
+            $event->theah->queueEvent($actionResolvedEvent);
+        }
+    }
+}
