c27976c5b6b7212a6db0e925e79dc005ead14688
Edward Mittelstedt <bugbucket@comcast.net>
2026-04-13 08:18:45 -0400

    Contempt and Hatred (01143) Audit

--- modules/php/cards/_7s5s/_01143.php
+++ modules/php/cards/_7s5s/_01143.php
@@ -106,15 +106,18 @@ class _01143 extends Scheme implements IHasActions
         if ($event instanceof EventCharacterRecruited && $this->Location == Game::LOCATION_PLAYER_HOME)
         {
             $character = $event->theah->getCharacterById($event->characterId);
-            $modifiedEvent = EventFactory::createCharacterInfluenceModifiedEvent(
-                $event->playerId, 
-                $event->characterId, 
-                $character->ModifiedInfluence, 
-                $character->ModifiedInfluence - 1,
-                $this->getInjectCode()
-            );
+            if ($character->hasTrait("Mercenary"))
+            {
+                $modifiedEvent = EventFactory::createCharacterInfluenceModifiedEvent(
+                    $event->playerId,
+                    $event->characterId,
+                    $character->ModifiedInfluence,
+                    $character->ModifiedInfluence - 1,
+                    $this->getInjectCode()
+                );
 
-            $event->theah->queueEvent($modifiedEvent);
+                $event->theah->queueEvent($modifiedEvent);
+            }
         }
     }
     
