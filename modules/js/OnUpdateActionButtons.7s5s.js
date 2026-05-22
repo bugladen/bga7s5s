f9d6959a8a3b5779e40b1051d7d4a19f0302e2e3
Edward Mittelstedt <bugbucket@comcast.net>
2026-05-22 17:43:25 -0400

    Porté Travel - Buttons made more clear so no misclicks made

--- modules/js/OnUpdateActionButtons.7s5s.js
+++ modules/js/OnUpdateActionButtons.7s5s.js
@@ -474,7 +474,7 @@
 
             'highDramaPhase01085': () => {
                 this.addActionButton(`actChooseCardSelected`, _('Confirm Selection'), () => this.onChooseInPlayCardConfirmed());
-                this.addActionButton(`actDone`, _('Done'), () =>  this.bgaPerformAction('actFromCardWithId', {id: 0}));
+                this.statusBar.addActionButton('No More Movement', () => this.bgaPerformAction('actFromCardWithId', {id: 0}), { id: 'actDone', color: 'alert' });
                 dojo.addClass('actChooseCardSelected', 'disabled');
             },
 
