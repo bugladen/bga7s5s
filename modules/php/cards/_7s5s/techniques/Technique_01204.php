88b94c6d63b76c18ddb15770d493311ffbe3761e
Edward Mittelstedt <bugbucket@comcast.net>
2026-09-07 12:10:11 -0400

    Syrneth Hand - now works with cards that copy Techniques (like Dame of Swords)

--- modules/php/cards/_7s5s/techniques/Technique_01204.php
+++ modules/php/cards/_7s5s/techniques/Technique_01204.php
@@ -3,6 +3,7 @@
 namespace Bga\Games\SeventhSeaCityOfFiveSails\cards\_7s5s\techniques;
 
 use Bga\Games\SeventhSeaCityOfFiveSails\cards\Attachment;
+use Bga\Games\SeventhSeaCityOfFiveSails\cards\Character;
 use Bga\Games\SeventhSeaCityOfFiveSails\cards\techniques\Technique;
 use Bga\Games\SeventhSeaCityOfFiveSails\EventFactory;
 use Bga\Games\SeventhSeaCityOfFiveSails\Game;
@@ -34,6 +35,31 @@ class Technique_01204 extends Technique
         return $inDuel;
     }
 
+    /**
+     * WHY: Dame of Swords (02055) / I Know That Trick (01165) clone this technique
+     * onto the participant Character. The original lives on Syrneth Hand and must
+     * stay attached; Character owners are valid copies and have no Attachment.
+     */
+    private function ownerCanApplyDeferredEffect(Theah $theah): bool
+    {
+        $owner = $this->getOwningCard($theah);
+        if ($owner instanceof Attachment)
+        {
+            return $owner->isAttached();
+        }
+
+        return $owner instanceof Character;
+    }
+
+    private function markOwnerUpdated(Theah $theah): void
+    {
+        $owner = $this->getOwningCard($theah);
+        if ($owner !== null)
+        {
+            $owner->IsUpdated = true;
+        }
+    }
+
     public function handleEvent(Event $event)
     { 
         parent::handleEvent($event);
@@ -41,39 +67,35 @@ class Technique_01204 extends Technique
         // If activated then this technique will reduce the opponent's Parry by 2 at the start of the next round
         if ($event instanceof EventResolveTechnique && $event->techniqueId == $this->Id)
         {
-            $attachment = $this->getOwningCard($event->theah);
+            $owner = $this->getOwningCard($event->theah);
             $character = $this->getOwningCharacter($event->theah);
-            $woundEvent = EventFactory::createCharacterBeingWoundedEvent($character->Id, $attachment->Id, 1, $attachment->getInjectCode(), $this->Id);
+            $woundEvent = EventFactory::createCharacterBeingWoundedEvent($character->Id, $owner->Id, 1, $owner->getInjectCode(), $this->Id);
             $event->theah->queueEvent($woundEvent);
 
             $this->ReduceAdversaryParry = true;
-            $attachment->IsUpdated = true;
+            $this->markOwnerUpdated($event->theah);
         }
 
         if ($event instanceof EventTechniqueCanceled && $event->techniqueId == $this->Id)
         {
             $this->ReduceAdversaryParry = false;
-            $attachment = $this->getOwningCard($event->theah);
-            if ($attachment instanceof Attachment)
-                $attachment->IsUpdated = true;
+            $this->markOwnerUpdated($event->theah);
         }
 
         //Reduce the opponent's Parry by 2 if the technique is activated
         if ($event instanceof EventDuelCalculateCombatCardStats && $this->ReduceAdversaryParry)
         {
-            $attachment = $this->getOwningCard($event->theah);
-            $isAttached = $attachment instanceof Attachment && $attachment->isAttached();
-
-            if ($isAttached)
+            if ($this->ownerCanApplyDeferredEffect($event->theah))
             {
+                $owner = $this->getOwningCard($event->theah);
                 $character = $this->getOwningCharacter($event->theah);
 
-                if ($character->Id == $event->adversaryId)
+                if ($character !== null && $character->Id == $event->adversaryId)
                 {
-                    $event->explanations[] = sprintf($event->theah->game->translate("%s reduces the Adversary's Parry by %d"), $attachment->getInjectCode(), 2);
+                    $event->explanations[] = sprintf($event->theah->game->translate("%s reduces the Adversary's Parry by %d"), $owner->getInjectCode(), 2);
                     $event->removeParry(2);
                     $this->ReduceAdversaryParry = false;
-                    $attachment->IsUpdated = true;
+                    $this->markOwnerUpdated($event->theah);
                 }
             }
         }
@@ -81,16 +103,13 @@ class Technique_01204 extends Technique
         // If the event is a new round and the owning character is the actor then reset the ReduceAdversaryParry flag
         if ($event instanceof EventDuelNewRound)
         {
-            $attachment = $this->getOwningCard($event->theah);
-            $isAttached = $attachment instanceof Attachment && $attachment->isAttached();
-
-            if ($isAttached)
+            if ($this->ownerCanApplyDeferredEffect($event->theah))
             {
                 $character = $this->getOwningCharacter($event->theah);
-                if ($character->Id == $event->actorId)
+                if ($character !== null && $character->Id == $event->actorId)
                 {
                     $this->ReduceAdversaryParry = false;
-                    $attachment->IsUpdated = true;
+                    $this->markOwnerUpdated($event->theah);
                 }
             }
         }
@@ -98,13 +117,10 @@ class Technique_01204 extends Technique
         // If the duel is over then reset the ReduceAdversaryParry flag
         if ($event instanceof EventDuelEnd)
         {
-            $attachment = $this->getOwningCard($event->theah);
-            $isAttached = $attachment instanceof Attachment && $attachment->isAttached();
-
-            if ($isAttached)
+            if ($this->ownerCanApplyDeferredEffect($event->theah))
             {
                 $this->ReduceAdversaryParry = false;
-                $attachment->IsUpdated = true;
+                $this->markOwnerUpdated($event->theah);
             }
         }
     }
