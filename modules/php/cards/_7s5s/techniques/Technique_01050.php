3f68350a1c3e1f78529176154d267f3a267890b5
Edward Mittelstedt <bugbucket@comcast.net>
2026-09-18 08:16:22 -0400

    Unsavory Salve - Technique now available during challenges

--- modules/php/cards/_7s5s/techniques/Technique_01050.php
+++ modules/php/cards/_7s5s/techniques/Technique_01050.php
@@ -24,12 +24,6 @@ class Technique_01050 extends Technique
             return false;
         }
         
-        $inDuel = $theah->game->globals->get(Game::IN_DUEL, false);
-        if (! $inDuel)
-        {
-            return false;
-        }
-
         $thrust = $theah->getCurrentRoundThrust();
         if ($thrust < 1)
         {
