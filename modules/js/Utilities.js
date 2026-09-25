5f7305507f950946854520689a7bfc7419c3dd3f
Edward Mittelstedt <bugbucket@comcast.net>
2026-09-25 11:29:45 -0400

    Mobile hover delay updated to 1 second

--- modules/js/Utilities.js
+++ modules/js/Utilities.js
@@ -69,11 +69,11 @@ return declare('seventhseacityoffivesails.utilities', null, {
             theme: '7sfs',
             placement: 'auto',
             zIndex: 10000,
-            // 500ms is long enough to distinguish from a selection tap, short enough
+            // 1000ms is long enough to distinguish from a selection tap, short enough
             // that checking card text on mobile still feels intentional.
-            // WHY doubled from 250: mobile players were accidentally opening hover
+            // WHY quadrupled from 250: mobile players were accidentally opening hover
             // text while trying to select cards; longer hold reduces false triggers.
-            touch: ['hold', 500],
+            touch: ['hold', 1000],
             popperOptions: {
                 strategy: 'fixed',
                 modifiers: [
